0abbbe3f7c3fe7417bc81031b274a7b21a79202b
AntonioTroncoso <atrobar0610@m.educaand.es>
2022-11-08 13:05:25 +0100

    first commit

---
+++ Day_2/variables.py
@@ -0,0 +1,24 @@
+first_name = 'Antonio'
+last_name = 'Troncoso'
+full_name = first_name + ' ' + last_name
+country = 'España'
+city = 'Jerez'
+age = 17
+year = 2022
+is_married = False
+is_true = True
+is_light_on = True
+language, email = 'Español', 'atrobar0610@gmail.com'
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(language))
+print(type(email))
